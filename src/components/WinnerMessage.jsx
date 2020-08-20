cb152fa0f89f88eb4f733257b4aafeeb5b66d934
daniel ghimpu <danielghimpu@yahoo.com>
2020-08-20 13:55:26 +0100

    Winner message component done

---
+++ src/components/WinnerMessage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WinnerMessage extends Component {
+  render() {
+    return (
+      <div className="win-msg">
+        <h1>Winner</h1>
+      </div>
+    );
+  }
+}
+
+export default WinnerMessage;
